cefaf78c0e0c7e9e8015445d188213e75ce36acb
Rustin Wollin <rwollin96@gmail.com>
2013-11-15 20:56:12 -0500

    re-implemented old camera system

--- js/graphics.js
+++ js/graphics.js
@@ -1,47 +1,46 @@
-var camera = {
-	x: 0,
-	y: 0,
-	panSpeed: player.runSpeed * 1.5,
-	step: function() {
-		switch(player.facing){
-			case 8:
-				var targetPos = (player.y+player.height/2)-(CANVAS_HEIGHT*0.6);
-				camera.y -= camera.panSpeed;
-				if(camera.y < targetPos) {
-					camera.y = targetPos;
-				}								
-				break;
-			case 2:
-				var targetPos = (player.y+player.height/2)-(CANVAS_HEIGHT*0.4);
-				camera.y += camera.panSpeed;
-				if(camera.y > targetPos) {
-					camera.y = targetPos;
-				}								
-				break;
-			case 4:
-				var targetPos = (player.x+player.width/2)-(CANVAS_WIDTH*0.6);
-				camera.x -= camera.panSpeed;
-				if(camera.x < targetPos) {
-					camera.x = targetPos;
-				}													
-				break;
-			case 6:
-				var targetPos = (player.x+player.width/2)-(CANVAS_WIDTH*0.4);
-				camera.x += camera.panSpeed;
-				if(camera.x > targetPos) {
-					camera.x = targetPos;
-				}							
-				break;
-		}
-
-		if(camera.x < 0) {
-			camera.x = 0;
-		}
+function updateCamera() {
+	var panSpeed = player.runSpeed * 1.5;
+	switch(player.facing){
+		case 8:
+			var targetPos = ((player.y+player.height/2)-(CANVAS_HEIGHT*0.6))*-1;
+			world.y += panSpeed;
+			if(world.y > targetPos) {
+				world.y = targetPos;
+			}								
+			break;
+		case 2:
+			var targetPos = ((player.y+player.height/2)-(CANVAS_HEIGHT*0.4))*-1;
+			world.y -= panSpeed;
+			if(world.y < targetPos) {
+				world.y = targetPos;
+			}
+			break;
+		case 4:
+			var targetPos = ((player.x+player.width/2)-(CANVAS_WIDTH*0.6))*-1;
+			world.x += panSpeed;
+			if(world.x > targetPos) {
+				world.x = targetPos;
+			}
+			break;
+		case 6:
+			var targetPos = ((player.x+player.width/2)-(CANVAS_WIDTH*0.4))*-1;
+			world.x -= panSpeed;
+			if(world.x < targetPos) {
+				world.x = targetPos;
+			}							
+			break;
 	}
+
+	if(world.x > 0) {
+		world.x = 0;
+	}
+
+	world.x = Math.round(world.x);
+	world.y = Math.round(world.y);
 }
 
-function draw() {
-	//camera.step();
 
+function draw() {
+	updateCamera();
 	stage.update();
 }
