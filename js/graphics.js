a76d6d5d140afcf40a9fbde017c1c3fad67288e5
Rustin Wollin <rwollin96@gmail.com>
2013-11-15 21:43:58 -0500

    camera now centers on player when not moving

--- js/graphics.js
+++ js/graphics.js
@@ -1,21 +1,31 @@
 function updateCamera() {
-	var panSpeedX = Math.abs(player.xv)*2;
-	var panSpeedY = 0;
-	switch(player.facing){
-		case 4:
-			var targetPos = ((player.x+player.width/2)-(CANVAS_WIDTH*0.6))*-1;
-			world.x += panSpeedX;
-			if(world.x > targetPos) {
-				world.x = targetPos;
-			}
-			break;
-		case 6:
-			var targetPos = ((player.x+player.width/2)-(CANVAS_WIDTH*0.4))*-1;
-			world.x -= panSpeedX;
-			if(world.x < targetPos) {
-				world.x = targetPos;
-			}
-			break;
+	if(player.xv == 0){
+		var targetPos = ((player.x+player.width/2)-(CANVAS_WIDTH*0.5))*-1;
+		if(world.x > targetPos) {
+			world.x -= (world.x-targetPos)*0.05;
+		}
+		if(world.x < targetPos) {
+			world.x -= (world.x-targetPos)*0.05;
+		}
+	} else {
+		var panSpeedX = Math.abs(player.xv)*2;
+		var panSpeedY = 0;
+		switch(player.facing){
+			case 4:
+				var targetPos = ((player.x+player.width/2)-(CANVAS_WIDTH*0.6))*-1;
+				world.x += panSpeedX;
+				if(world.x > targetPos) {
+					world.x = targetPos;
+				}
+				break;
+			case 6:
+				var targetPos = ((player.x+player.width/2)-(CANVAS_WIDTH*0.4))*-1;
+				world.x -= panSpeedX;
+				if(world.x < targetPos) {
+					world.x = targetPos;
+				}
+				break;
+		}	
 	}
 
 	if(world.x > 0) {
